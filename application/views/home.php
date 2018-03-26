2c9c1cb52dc265314dfd3541d960712e038b80a9
AnggunWJ <anggunwidiyaferijanitra@gmail.com>
2018-03-26 22:14:20 +0700

    awal tugas

---
+++ application/views/home.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+  <title>TUGAS2 CI</title>
+  <link rel="stylesheet" media="all"  type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
+<link rel="stylesheet" media="all"  type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.css">
+<link rel="stylesheet" media="all"  type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap-theme.css">
+  <script type="text/javascript" src="<?php echo base_url() ?>assets/js/jquery.js"></script>
+  <script type="text/javascript" src="<?php echo base_url() ?>assets/js/bootstrap.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">FERI</a>
+      </div>
+      <ul class="nav navbar-nav">
+          <li class="active"><a href="<?php echo site_url() ?>home">Home</a></li>
+        <li><a href="<?php echo site_url() ?>about">About</a></li>
+        <li><a href="<?php echo site_url() ?>contact">Contact</a></li> 
+        
+      </ul>
+     
+    </div>
+  </nav>
+<h1>Home 1</h1>
+</body>
+</html>
