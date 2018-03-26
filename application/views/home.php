64c28285f3edd55a5c0966576a6066a40c48b330
AnggunWJ <anggunwidiyaferijanitra@gmail.com>
2018-03-26 23:35:01 +0700

    update tugas

--- application/views/home.php
+++ application/views/home.php
@@ -14,7 +14,7 @@
         <a class="navbar-brand" href="#">FERI</a>
       </div>
       <ul class="nav navbar-nav">
-          <li class="active"><a href="<?php echo site_url() ?>home">Home</a></li>
+        <li class="active"><a href="<?php echo site_url() ?>home">Home</a></li>
         <li><a href="<?php echo site_url() ?>about">About</a></li>
         <li><a href="<?php echo site_url() ?>contact">Contact</a></li> 
         
@@ -22,6 +22,19 @@
      
     </div>
   </nav>
-<h1>Home 1</h1>
+  <h1>Juventus Sekarang Kembali Fokus ke Serie A</h1> 
+  <h4>Bola.net - Giorgio Chiellini menyatakan bahwa Juventus kembali fokus ke Serie A setelah tengah pekan kemarin bermain di Liga Champions. Ia memastikan timnya ingin merebut posisi pertama klasemen yang saat ini diduduki Napoli.
+
+<br>Akhir pekan ini Bianconeri akan menghadapi Udinese di kandang sendiri. Kemenangan akan menggeser Napoli di peringkat pertama, karena jarak antara kedua tim sekarang ini hanya terpaut satu poin. Sementara Napoli sendiri akan bermain di pekan ke-28 menghadapi Inter Milan beberapa jam setelah Juventus berlaga.
+
+<br>Sebelum bertemu dengan Udinese, pasukan Massimiliano Allegri memetik kemenangan penting kala menghadapi Tottenham di Liga Champions. Kemenangan itu membuat klub perwakilan dari Italia tersebut lolos babak delapan besar.
+
+<br>Chiellini mengatakan Juve perlu melupakan sementara soal laga di Liga Champions. Ia ingin Juventus kembali fokus di Serie A karena sedang berebut tempat pertama dengan Napoli.
+
+<br>"Kami berharap merebut posisi paling atas, mungkin akhir pekan ini meskipun hanya untuk beberapa jam saja," ucap Chiellini pada laman resmi klub.
+
+<br>"Terdapat tiga laga yang sulit menghadapi tim yang mengandalkan fisik di akhir periode sibuk ini. Kami membutuhkan seluruh kekuatan fisik dan energi dan berkonsentrasi yang baik. Kami harus melupakan Liga Champions karena beberapa hari ke depan sanga penting," sambungnya. </h4> 
+ 
+ 
 </body>
 </html>
